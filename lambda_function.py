7e2327cc1f89ca6c2a67655917031cdc19ae94f8
Sean Hannan <seanphannan@gmail.com>
2023-01-07 11:49:57 -0500

    Update lambda_function.py to use a Scrapy
    Now uses Scrapy instead of urlllib + BeautifulSoup. Faster for pulling the data and parsing it. Requires the Scrapy Lambda layer in AWS.

--- lambda_function.py
+++ lambda_function.py
@@ -1,81 +1,62 @@
-import urllib.request
-from bs4 import BeautifulSoup as soup
-import time
+import scrapy
+import scrapydo
 import json
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy import Item, Field
 
-# Parses out the id, link, name, rating, and heart status for a user's movies
-def parse_page(page_html):
-    movies = []
-    page_soup = soup(page_html, 'html.parser')
+class FilmItem(Item):
+    id = Field()
+    name = Field()
+    url = Field()
+    rating = Field()
+    heart = Field()
 
-    for id in page_soup.find_all('li', class_ = 'poster-container'):
-        identifier = id.contents[1]['data-film-id']
-        link = id.contents[1]['data-target-link']
-        name  = id.contents[1].contents[1]['alt']
-        try:
-            rating = [i.split('rated-')[1] for i in id.contents[3].contents[1].attrs['class'] if i.startswith('rated-')][0]
-        except IndexError:
-            rating = 0
-        try:
-            if id.contents[3].contents[3] != '':
+
+class FilmSpider(CrawlSpider):
+    name = "film_spider"
+    allowed_domains = ['letterboxd.com']
+    rules = [Rule(LinkExtractor(allow=['films/page']), callback='parse_item', follow=True)]
+
+    # Parses out the id, link, name, rating, and heart status for a user's movies
+    def parse_item(self, response):
+        for id in response.css('li.poster-container'):
+            identifier = id.css('::attr(data-film-id)').get()
+            link = id.css('::attr(data-target-link)').get()
+            name  = id.css('::attr(alt)').get()
+            try:
+                rating = id.css('p.poster-viewingdata span.rating').css('::attr(class)').re(r'\brated-(\d+)\b')[0]
+            except IndexError:
+                rating = 0
+            if id.css('p.poster-viewingdata span.like').get() != None:
                 heart = 1
-        except IndexError:
-            heart = 0
+            else:
+                heart = 0
 
-        movie = {
-            'id': int(identifier),
-            'name': name,
-            'url': link,
-            'rating': int(rating),
-            'heart': heart
-        }
+            item = FilmItem()
+            item["id"] = int(identifier)
+            item["name"] = name
+            item["url"] = link
+            item["rating"] = int(rating)
+            item["heart"] = heart
 
-        movies.append(movie)
-    
-    return movies
+            yield item
 
-# Parses out the last page number of the user
-def get_last_page(page_html):
-    page_soup = soup(page_html, 'html.parser')
-    page_links = page_soup.find_all('li', class_ = 'paginate-page')
-
-    if not page_links:
-        return 0
-
-    last_page = page_links[-1].contents[0].get_text()
-
-    return last_page
 
 def lambda_handler(event, context):
     # Set up variables and grab the initial page
     user = event['queryStringParameters']['user']
-    url = 'https://letterboxd.com/' + user + '/films/'
+    url = 'https://letterboxd.com/' + user + '/films/page/1'
 
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
-    request = urllib.request.Request(url, headers=headers)
-    response = urllib.request.urlopen(request)
-    page_html = response.read()
-
-    # Parse the page and also parse out the last page number
-    movies  = parse_page(page_html)
-    last_page = get_last_page(page_html)
-
-    # If there's only one page, return
-    if int(last_page) == 0:
-        return json.dumps(movies)
-
-    # Loop through the rest of the page parsing and adding to the array
-    for page in range(2, int(last_page) + 1):
-        url = 'https://letterboxd.com/' + user + '/films/page/' + str(page)
-        request = urllib.request.Request(url, headers=headers)
-        response = urllib.request.urlopen(request)
-        page_html = response.read()
-
-        time.sleep(5)
-
-        movies += parse_page(page_html)
+    # Set up the Scrapy spider and crawl through the pages
+    scrapydo.setup()
+    items = scrapydo.run_spider(FilmSpider, start_urls=[url])
     
-    return json.dumps(movies)
-
-
+    jsonText = '[';
+    for item in items:
+        jsonText += json.dumps(dict(item)) + ","
     
+    jsonText = jsonText[:-1]
+    jsonText += ']'
+    
+    return jsonText
